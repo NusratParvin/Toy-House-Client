555c483e5ac882e77527862fb3d8ec7be245872e
NusratParvin <nusrat.official@gmail.com>
2023-05-19 22:41:41 +0400

    gallery

--- src/pages/Gallery/Gallery.jsx
+++ src/pages/Gallery/Gallery.jsx
@@ -4,42 +4,59 @@ import img3 from '../../assets/3.jpg';
 import img4 from '../../assets/4.jpg';
 import img5 from '../../assets/5.jpg';
 import img6 from '../../assets/6.jpg';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from 'react';
 
 const Gallery = () => {
+    useEffect(() => {
+        AOS.init();
+      }, [])
     return (
         <div>
             <div className="w-full mx-auto px-5 py-16 mb-10 lg:px-32 lg:pt-24">
                <h1 className='text-4xl text-zinc-600  tracking-tighter py-4 mb-6 text-center font-bold'>What We Have In Stock</h1>
                 <div className="-m-1 flex flex-wrap md:-m-2">
                     <div className="flex w-1/2 flex-wrap">
-                        <div className="w-1/2 p-1 md:p-1">
+                        <div className="w-1/2 p-1 md:p-1" data-aos='fade-right'
+                        data-aos-offset="100"
+                        data-aos-easing="ease-in-sine">
                             <img
-                                className="block h-full w-full  object-cover object-center animate-pulse duration-900"
+                                className="block h-full w-full  object-cover object-center"
                                 src={img1}/>
                         </div>
-                        <div className="w-1/2 p-1 md:p-1">
+                        <div className="w-1/2 p-1 md:p-1" data-aos='fade-up' data-aos-offset="200"
+     data-aos-easing="ease">
                             <img
                                 className="block h-full w-full object-cover object-center"
                                 src={img2}/>
                         </div>
-                        <div className="w-full p-1 md:p-1">
+                        <div className="w-full p-1 md:p-1" data-aos="fade-right"
+     data-aos-offset="250"
+     data-aos-easing="ease-in-sine">
                             <img
                                 className="block h-full w-full object-cover object-center"
                                 src={img4} />
                         </div>
                     </div>
                     <div className="flex w-1/2 flex-wrap">
-                        <div className="w-full p-1 md:p-1">
+                        <div className="w-full p-1 md:p-1 "  data-aos="fade-left"
+     data-aos-offset="350"
+     data-aos-easing="ease-in">
                             <img
                                 className="block h-full w-full object-cover object-center"
                                 src={img3}/>
                         </div>
-                        <div className="w-1/2 p-1 md:p-1">
+                        <div className="w-1/2 p-1 md:p-1"  data-aos="zoom-in-up"
+     data-aos-offset="100"
+     data-aos-easing="ease-in-sine">
                             <img
                                 className="block h-full w-full object-cover object-center"
                                 src={img5} />
                         </div>
-                        <div className="w-1/2 p-1 md:p-1">
+                        <div className="w-1/2 p-1 md:p-1" data-aos="fade-up-left"
+     data-aos-offset="100"
+     data-aos-easing="ease-in-sine">
                             <img
                                 className="block h-full w-full object-cover object-center"
                                 src={img6} />
